22bf671f44af34d89de79849b45ea220a2446f11
Alex Gras <a.gras@avanade.com>
2022-06-05 19:36:30 +0200

    28-asyncScheduler

---
+++ src/observables/08-asyncScheduler.ts
@@ -0,0 +1,33 @@
+
+
+import { asyncScheduler } from 'rxjs';
+
+setTimeout ( () => {}, 3000);
+setInterval( () => {}, 3000);
+
+
+const saludar = () => console.log('Hola Mundo');
+// asyncScheduler.schedule( saludar, 2000);
+
+const saludar2 = nombre => console.log(`Hola ${ nombre }`);
+// asyncScheduler.schedule( saludar2, 2000, 'Alex');
+
+const subs = asyncScheduler.schedule( function(state) {
+    console.log('state: ', state);
+
+    this.schedule( state +1, 1000);
+}, 3000, 0);
+
+// setTimeout(() => {
+//     subs.unsubscribe();
+// }, 6000);
+
+asyncScheduler.schedule( () => subs.unsubscribe(), 6000 );
+
+
+
+
+
+
+
+
