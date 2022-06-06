377406b8b577904b6b9cc1cfdb06263101472829
Alex Gras <a.gras@avanade.com>
2022-06-06 09:33:45 +0200

    35-map, pluck

---
+++ src/observables/09-from-avanzado.ts
@@ -0,0 +1,47 @@
+
+import { of, from, Observer } from 'rxjs';
+
+/**
+ * of = toma argumentos y genera una secuencia
+ * from= array, promise, iterable, observable
+ */
+
+
+const observer = {
+    next: value => console.log('next: ', value),
+    complete: () => console.log('complete')
+};
+
+const miGenerador = function*() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+    yield 6;
+    yield 7;
+    yield 8;
+}
+
+const miIterable = miGenerador();
+
+// for(let id of miIterable){
+//     console.log(id);
+// }
+
+from( miIterable ).subscribe( observer );
+
+// const source$ = from([1,2,3,4,5]);
+// const source$ = of(...[1,2,3,4,5]);
+// const source$ = from('Fernando');
+
+const source$ = from ( fetch('https://api.github.com/users/klerith') );
+
+// source$.subscribe( async(resp) => {
+
+//     console.log( resp.ok );
+//     const dataResp = await resp.json();
+//     console.log(dataResp);
+// });
+
+// source$.subscribe( observer ); 
