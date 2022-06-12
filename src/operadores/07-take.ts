d067bbca0661e3a036e66fb8fdd010d4cb9458cd
Alex Gras <a.gras@avanade.com>
2022-06-12 09:54:12 +0200

    47-take

---
+++ src/operadores/07-take.ts
@@ -0,0 +1,15 @@
+import { of } from "rxjs";
+import { take, tap } from 'rxjs/operators';
+
+
+const numeros$ = of(1,2,3,4,5).pipe(
+    tap( console.log )
+);
+
+numeros$.pipe(
+    tap( t => console.log('tap: ', t) ),
+    take(3)
+).subscribe({
+    next: val => console.log('val: ', val),
+    complete: () => console.log('complete')
+})
