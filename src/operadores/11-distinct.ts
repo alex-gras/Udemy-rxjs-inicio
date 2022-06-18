f3aa9b8b43e58c357571a071dce45e4e27d9aecf
Alex Gras <a.gras@avanade.com>
2022-06-18 19:52:29 +0200

    53-distinctUntilChange

--- src/operadores/11-distinct.ts
+++ src/operadores/11-distinct.ts
@@ -1,4 +1,13 @@
-import { distinct, of, from, distinctUntilChanged } from 'rxjs';
+import { distinct, of, from } from 'rxjs';
+
+
+const numeros$ = of(1,'1',1,3,3,2,2,4,4,5,3,1, '1');
+
+
+numeros$.pipe(
+        distinct()
+    ).subscribe( console.log );
+
 
 interface Personaje {
     nombre: string;
@@ -9,7 +18,7 @@ const personajes: Personaje[] = [
         nombre: 'Megaman'
     },
     {
-        nombre: 'Megaman'
+        nombre: 'X'
     },
     {
         nombre: 'Zero'
@@ -21,7 +30,7 @@ const personajes: Personaje[] = [
         nombre: 'X'
     },
     {
-        nombre: 'X'
+        nombre: 'Megaman'
     },
     {
         nombre: 'Zero'
@@ -29,7 +38,7 @@ const personajes: Personaje[] = [
 ];
 
 from( personajes ).pipe(
-    distinctUntilChanged((ant, act) => ant.nombre===act.nombre )
+    distinct( p => p.nombre )
 ).subscribe( console.log );    
 
 
--- src/operadores/12-distinctUntilChange.ts
+++ src/operadores/12-distinctUntilChange.ts
@@ -1,5 +1,15 @@
 import { distinct, of, from, distinctUntilChanged } from 'rxjs';
 
+
+const numeros$ = of(1,'1',1,3,3,2,2,4,4,5,3,1, '1');
+
+
+numeros$.pipe(
+        // distinct()
+        distinctUntilChanged()
+    ).subscribe( console.log );
+
+
 interface Personaje {
     nombre: string;
 }
@@ -29,6 +39,7 @@ const personajes: Personaje[] = [
 ];
 
 from( personajes ).pipe(
+    // distinct( p => p.nombre )
     distinctUntilChanged((ant, act) => ant.nombre===act.nombre )
 ).subscribe( console.log );    
 
